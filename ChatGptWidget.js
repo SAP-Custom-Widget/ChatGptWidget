7abf65faa972da74b83453354e19a6637ddf695c
Rohit Chouhan <34239087+rohit-chouhan@users.noreply.github.com>
2023-04-05 14:01:42 +0530

    Update ChatGptWidget.js

--- ChatGptWidget.js
+++ ChatGptWidget.js
@@ -100,11 +100,18 @@
             "temperature": 0.5
           })
         });
-        const {
-          choices
-        } = await response.json();
-        const generatedTextValue = choices[0].text;
-        generatedText.value = generatedTextValue.replace(/^\n+/, '');
+
+        if (response.status === 200) {
+          const {
+            choices
+          } = await response.json();
+          const generatedTextValue = choices[0].text;
+          generatedText.value = generatedTextValue.replace(/^\n+/, '');
+        } else {
+          const error = await response.json();
+          alert("OpenAI Response: " + error.error.message);
+          generatedText.value = "";
+        }
       });
     }
     onCustomWidgetBeforeUpdate(changedProperties) {
